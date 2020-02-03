f9ca9a866af4a36cfa4bfd9c8ab976b8cb2f2844
João Dias <contacto@joaodias.me>
2020-02-03 22:21:47 +0000

    Related

--- src/components/row/index.js
+++ src/components/row/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 export class Row extends Component {
   /**
@@ -7,38 +7,40 @@ export class Row extends Component {
    * @param {string[]} tags
    */
   renderTags(tags) {
-    const list = tags.map(tag => (
-      <span className="controls__tags__item">{tag}</span>
-    ));
+    const list = tags.map((tag, index) => {
+      return (
+        <span key={index} className="controls__tags__item">
+          {tag}
+        </span>
+      );
+    });
 
     return <div className="controls__tags">{list}</div>;
   }
 
   /**
-   * Renders the carousel of videos
    *
-   * @returns {JSX.Element}
+   * @param {*} item
+   * @param {*} index
    */
-  renderCarousel() {
-    const { data } = this.props;
-    const list = data.map((item, index) => this.getCarouselItems(item, index));
-
-    return <ul className="section__carousel">{list}</ul>;
-  }
-
   getCarouselItems(item, index) {
     const { id, name, to, title, age, duration, tags } = item;
 
     return (
       <li key={id} className="section__item" style={{ "--order": `${index}` }}>
         <label htmlFor={id}>
-          <input id={id} name={name} className="section__item__input" />
-          <figure className="section__item__background" />
-          <Link to={{ to }} className="section__item__controls">
+          <input
+            type="radio"
+            id={id}
+            name={name}
+            className="section__item__input"
+            value=""
+          />
+          <Link to={to} className="section__item__controls">
             <figure className="controls__icon" />
             <h4 className="controls__title">{title}</h4>
             <div className="controls__metadata">
-              <span className="controls__age">{age}+</span>
+              <span className="controls__age">{age}</span>
               <span className="controls__duration">{duration}</span>
             </div>
             {this.renderTags(tags)}
@@ -48,8 +50,29 @@ export class Row extends Component {
     );
   }
 
+  /**
+   * Renders the carousel of videos
+   *
+   * @returns {JSX.Element}
+   */
+  renderCarousel() {
+    const { data } = this.props;
+
+    if (data) {
+      const list = data.map((item, index) =>
+        this.getCarouselItems(item, index)
+      );
+
+      return <ul className="section__carousel">{list}</ul>;
+    }
+
+    return <p>...loading</p>;
+  }
+
   render() {
     const { id, className, title } = this.props;
+
+    console.log("history: ", this.props);
 
     return (
       <section id={id} className={className}>
--- src/data/related.js
+++ src/data/related.js
@@ -1,4 +1,4 @@
-import { MOVIE_DETAILS } from "../../";
+import { MOVIE_DETAILS } from "../constants/index";
 
 export const RELATED = {
   id: "803a5589-d36a-4e94-a821-1eb32539c896",
@@ -11,6 +11,15 @@ export const RELATED = {
       title: "American Sniper",
       age: "16",
       duration: "2h12m",
+      tags: ["Gritty", "Action & Adventure", "Drama"],
+    },
+    {
+      id: "9ef63bf8-86a1-423f-9c97-97cfc997e6a0",
+      name: "related-movies",
+      to: `${MOVIE_DETAILS}/9ef63bf8-86a1-423f-9c97-97cfc997e6a0`,
+      title: "Fury",
+      age: "16",
+      duration: "1h30m",
       tags: ["Gritty", "Action & Adventure", "Drama"],
     },
   ],
